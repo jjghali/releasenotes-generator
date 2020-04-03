ed94d53e25721316a93c401e5df0253398203e5b
Joshua Ghali <joshua.ghali@desjardins.com>
2020-04-03 16:45:34 -0400

    added confluence releasenotes template

--- src/templates/confluence.template.ts
+++ src/templates/confluence.template.ts
@@ -1,4 +1,12 @@
 const releaseNoteTemplate: string = `
+h4. Tickets
+%1%s
+{expand:Cas de Tests}\n
+{jiraissues:anonymous=false |columns=key,status | jqlQuery=issuetype=Test AND issuefunction in linkedIssuesof("key in (%2$s)")}
+{expand}
+{expand:Téléchargements}\n
+%3%s
+{expand}
 `;
 const componentsNoteTemplate: string = `
 `;
