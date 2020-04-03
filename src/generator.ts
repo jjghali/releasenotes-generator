02619a7d8ea7873b1dd2296217f7a711dbd31da8
Joshua Ghali <joshua.ghali@desjardins.com>
2020-04-03 18:02:33 -0400

    added character encoding

--- src/generator.ts
+++ src/generator.ts
@@ -1,5 +1,6 @@
 const sprintf = require("sprintf-js").sprintf;
 const markdownTemplate = require("./templates/markdown.template");
+const confluenceTemplate = require("./templates/confluence.template");
 import { markdownToAtlassianWikiMarkup } from "@kenchan0130/markdown-to-atlassian-wiki-markup";
 import { RepositoryTag } from "./model/repositoryTag.model";
 import { JiraTask } from "./model/jiraTask.model";
@@ -63,11 +64,65 @@ class Generator {
   }
 
   public generateConfluenceFormat(): string {
-    if (this.mdReleaseNotes == "") {
-      this.generateMarkdown();
+    const tasksList: string = this.generateConfluenceTaskList(
+      this.repositoryTag.jiraTasks
+    );
+
+    const downloadSection: string = this.generateConfluenceDownloadSection(
+      this.repositoryTag.artifactVersion
+    );
+
+    const testCasesSection: string = this.generateConfluenceTestCases(
+      this.repositoryTag.jiraTasks
+    );
+
+    let releaseNote: string = sprintf(
+      confluenceTemplate.releaseNoteTemplate,
+      tasksList,
+      testCasesSection,
+      downloadSection
+    );
+    return releaseNote;
+  }
+
+  private generateConfluenceTaskList(jiraTasks: JiraTask[]): string {
+    let taskList: string = "";
+
+    jiraTasks.forEach((j: JiraTask) => {
+      taskList +=
+        "**[" +
+        j.key +
+        "|https://jira.desjardins.com/browse/" +
+        j.key +
+        "]:* " +
+        j.summary +
+        " (" +
+        j.summary +
+        ")\n";
+    });
+    return taskList;
+  }
+
+  private generateConfluenceDownloadSection(
+    artifactVersion: ArtifactVersion
+  ): string {
+    let downloadSection: string = "";
+
+    artifactVersion.files.forEach((f: ArtifactFile) => {
+      downloadSection += `* [` + f.name + `\\` + f.downloadURL + `]\n`;
+    });
+    return downloadSection;
+  }
+
+  private generateConfluenceTestCases(jiraTasks: JiraTask[]): string {
+    let taskList: string = "";
+
+    for (let i = 0; i < jiraTasks.length; i++) {
+      const j = jiraTasks[i];
+      taskList += j.key;
+      if (i + 1 != jiraTasks.length) taskList += ",";
     }
-    const confluenceMarkup = markdownToAtlassianWikiMarkup(this.mdReleaseNotes);
-    return confluenceMarkup;
+    return taskList;
   }
 }
 export { Generator };
--- src/templates/confluence.template.ts
+++ src/templates/confluence.template.ts
@@ -1,11 +1,11 @@
 const releaseNoteTemplate: string = `
 h4. Tickets
-%1%s
-{expand:Cas de Tests}\n
-{jiraissues:anonymous=false |columns=key,status | jqlQuery=issuetype=Test AND issuefunction in linkedIssuesof("key in (%2$s)")}
+%1$s
+{expand:Cas de Tests}
+{jiraissues:anonymous=false |columns=key,status | jqlQuery=issuetype=Test AND issuefunction in linkedIssuesof(\"key in (%2$s)\")}
 {expand}
-{expand:Téléchargements}\n
-%3%s
+{expand:Téléchargements}
+%3$s
 {expand}
 `;
 const componentsNoteTemplate: string = `
